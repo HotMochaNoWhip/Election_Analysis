9ae68cd1be31d357ad263bf87f4291436fc31099
Mo <morganipeterson7@gmail.com>
2021-09-05 22:30:16 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -7,14 +7,16 @@
 
 #Project Start
 import csv
+import os
 
 # Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Open the election results and read the file.
-election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
 
-# To do: perform analysis.
+     # To do: perform analysis.
+     print(election_data)
 
 # Close the file.
 election_data.close()
